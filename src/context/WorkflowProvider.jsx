5320057e7bc1baea5c73f4dd5e3f765d6a5fb72c
adamsartell <adam.sartell@gmail.com>
2025-07-03 12:29:17 -0700

    remove unused useeffect import from workflow provider

--- src/context/WorkflowProvider.jsx
+++ src/context/WorkflowProvider.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { WorkflowContext } from "./WorkflowContext";
 import {
 	saveWorkflowSelectionsToLocalStorage,
